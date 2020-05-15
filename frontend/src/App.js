6cdfa5ec4d5fa1aff9171ae7e9e0288e365d8301
Jan Heuermann <jan@jotaen.net>
2020-05-15 12:37:35 +0200

    Change code style (tabs and no semicolons)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
 	Link
-} from 'react-router-dom';
-import { Home } from './Home';
-import { Exercises } from './Exercises';
-import logo from './logo.svg';
+} from 'react-router-dom'
+import { Home } from './Home'
+import { Exercises } from './Exercises'
+import logo from './logo.svg'
 
 const ROUTES = {
-  HOME: '/',
-  EXERCISES: '/exercises',
-};
-
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Router>
-          <>
-            <nav className='Navigation'>
-              <img src={logo} className='App-logo' alt='logo' />
-              <ul>
-                <li>
-                  <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                  <Link to={ROUTES.EXERCISES}>Exercises</Link>
-                </li>
-              </ul>
-            </nav>
-
-            <Switch>
-              <Route path={ROUTES.EXERCISES}>
-                <Exercises />
-              </Route>
-              <Route path={ROUTES.HOME}>
-                <Home />
-              </Route>
-            </Switch>
-          </>
-        </Router>
-      </header>
-    </div>
-  );
+	HOME: '/',
+	EXERCISES: '/exercises',
 }
 
-export default App;
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Router>
+					<>
+						<nav className='Navigation'>
+							<img src={logo} className='App-logo' alt='logo' />
+							<ul>
+								<li>
+									<Link to={ROUTES.HOME}>Home</Link>
+								</li>
+								<li>
+									<Link to={ROUTES.EXERCISES}>Exercises</Link>
+								</li>
+							</ul>
+						</nav>
+
+						<Switch>
+							<Route path={ROUTES.EXERCISES}>
+								<Exercises />
+							</Route>
+							<Route path={ROUTES.HOME}>
+								<Home />
+							</Route>
+						</Switch>
+					</>
+				</Router>
+			</header>
+		</div>
+	)
+}
+
+export default App
