5906c3920e10d4eccb55d7801813bd34f300591e
Jan Heuermann <jan@jotaen.net>
2020-05-15 14:05:41 +0200

    Setup nginx for static file serving

---
+++ ops/bootstrap-server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##############################################################
+### Bootstrap a CentOS 8 Server
+### This script is supposed to be run under the `root` user
+##############################################################
+
+#
+#	Basic setup of operating system
+#
+dnf update -y
+
+#
+# Install `docker` and `docker-compose`
+#
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce --nobest
+systemctl start docker
+systemctl enable docker
+
+curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+#
+# Install `git`
+#
+dnf install -y git
+mkdir /repository.git
+cd /repository.git
+git --bare init
+
+
