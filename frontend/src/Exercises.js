801544b155e23359d93a4269ea3610db05bdff41
Jan Heuermann <jan@jotaen.net>
2020-05-15 11:58:44 +0200

    Use `react-router` for views

---
+++ frontend/src/Exercises.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Exercises() {
+  return <>
+    <h1>Exercises</h1>
+    <ul>
+      <li>Mechanics</li>
+      <li>Thermodynamics</li>
+    </ul>
+  </>;
+}
