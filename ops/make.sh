76835b6bf1133be30b5072722a60912be1d0217e
Jan Heuermann <jan@jotaen.net>
2020-05-21 13:27:58 +0200

    Pack app modules into containers

---
+++ ops/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build \
+	--no-cache \
+	--build-arg REACT_APP_API_ADDRESS=http://api.step-wise.com \
+	--tag=frontend_build \
+	../frontend
+
+docker-compose build \
+	--no-cache
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-	-v $(pwd)/frontend:/app \
-	-w /app \
-	-e CI=true \
-	node:12.16.3-alpine npm test
