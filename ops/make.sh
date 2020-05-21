f2a5eda3184bc66fb52142b19f2076c80c5d0fc0
Jan Heuermann <jan@jotaen.net>
2020-05-21 14:13:04 +0200

    Suppress verbose output during npm install

--- ops/make.sh
+++ ops/make.sh
@@ -2,7 +2,7 @@
 
 docker build \
 	--no-cache \
-	--build-arg REACT_APP_API_ADDRESS=http://api.step-wise.com \
+	--build-arg REACT_APP_API_ADDRESS='http://api.step-wise.com' \
 	--tag=frontend_build \
 	../frontend
 
