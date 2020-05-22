25c947f2a21385c5ea634d60154660f0e703b38e
Jan Heuermann <jan@jotaen.net>
2020-05-22 12:15:06 +0200

    Setup database connection with `sequelize`

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,20 @@
 require('dotenv').config()
 const { server } = require('./server')
+const { Sequelize } = require('sequelize')
 
-server.listen({ port: process.env.PORT }).then(({ port }) => {
-	console.log(`Server listening on port ${port}`)
+const sequelize = new Sequelize(
+	process.env.POSTGRES_DB,
+	process.env.POSTGRES_USER,
+	process.env.POSTGRES_PASSWORD,
+	{
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		dialect: 'postgres',
+		logging: process.env.NODE_ENV === 'development' ? console.log : false,
+})
+
+sequelize.authenticate().then(async () => {
+	server.listen({ port: process.env.PORT }).then(({ port }) => {
+		console.log(`Server listening on port ${port}`)
+	})
 })
