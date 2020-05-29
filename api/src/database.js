e219d1aecc1d257ed635759fe2f9ab76e0667034
Jan Heuermann <jan@jotaen.net>
2020-05-29 13:57:01 +0200

    Wire up openid as POC

--- api/src/database.js
+++ api/src/database.js
@@ -4,27 +4,21 @@ const { DataTypes } = require('sequelize')
 class Database extends DataSource {
 	constructor(sequelizeInstance) {
 		super()
-		this.sequelize = sequelizeInstance
-		this.models = {
-			Exercise: this.sequelize.define('Exercise', {
-				name: {
-					type: DataTypes.STRING,
-					allowNull: false,
-				}
-			})
-		}
+		this._sequelize = sequelizeInstance
+		this.Exercise = this._sequelize.define('Exercise', {
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			}
+		})
 	}
 
 	async dangerouslySyncDatabaseSchema() {
 		// TODO Don’t use syncing in production, because it might erase entire tables.
 		// Introduce proper migrations at some point!
 		// See https://sequelize.org/master/manual/model-basics.html#model-synchronization
-		await this.sequelize.sync({ alter: true })
+		await this._sequelize.sync({ alter: true })
 		return this
-	}
-
-	async getAllExercises() {
-		return await this.models.Exercise.findAll()
 	}
 }
 
