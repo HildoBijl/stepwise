ad3ae516ce0aad9b15127bd1d6dcbaee9e58e9db
Hildo Bijl <hildobijl@gmail.com>
2020-05-31 16:25:44 +0200

    Remove outdated React components

--- frontend/src/ui/pages/index.js
+++ frontend/src/ui/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Home } from './Home'
-export { default as Exercises } from './Exercises.js' // TODO REMOVE
 export { default as Courses } from './Courses.js'
 export { default as Course } from './Course.js'
 export { default as Feedback } from './Feedback.js'
