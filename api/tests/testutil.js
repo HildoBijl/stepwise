8bd407ae3adc349c0c849adb83e6bf314c937761
Jan Heuermann <jan@jotaen.net>
2020-10-16 14:04:44 +0200

    Variable naming

--- api/tests/testutil.js
+++ api/tests/testutil.js
@@ -1,8 +1,8 @@
 // You can only run this when you have permissions to
 // change the schema. In a shared production DB you might
 // need to drop the database manually instead.
-const clearDatabaseSchema = async (sequelizeTemplateDb) => {
-	await sequelizeTemplateDb.query(`
+const clearDatabaseSchema = async (sequelize) => {
+	await sequelize.query(`
 		drop schema if exists public cascade;
 		create schema public;
 	`)
