13f59503938e4254ede4ea8f9020ff0c27b3d0b9
Hildo Bijl <hildobijl@gmail.com>
2021-02-14 14:38:03 +0100

    Add second useIsentropicEfficiency exercise

---
+++ frontend/src/ui/edu/exercises/exercises/useIsentropicEfficiencyCompressor2.js
@@ -0,0 +1,113 @@
+import React from 'react'
+
+import { M, BM } from 'ui/components/equations'
+import { Par, SubHead } from 'ui/components/containers'
+import FloatUnitInput from 'ui/form/inputs/FloatUnitInput'
+import { InputSpace } from 'ui/form/Status'
+
+import StepExercise from '../types/StepExercise'
+import { useCorrect } from '../ExerciseContainer'
+import { getDefaultFeedback } from '../util/feedback'
+
+export default function Exercise() {
+	return <StepExercise Problem={Problem} steps={steps} getFeedback={getFeedback} />
+}
+
+const Problem = ({ p1, p2, T1, etai }) => <>
+	<Par>Een compressor in een gasturbine comprimeert lucht van <M>{p1}</M> en <M>{T1}</M> tot <M>{p2}.</M> Deze compressie verloopt niet isentroop: het isentropisch rendement is <M>{etai}.</M> Bereken de temperatuur van de lucht aan de uitgang van de compressor.</Par>
+	<InputSpace>
+		<Par>
+			<FloatUnitInput id="T2" prelabel={<M>T_2 =</M>} label="Temperatuur na compressor" size="s" />
+		</Par>
+	</InputSpace>
+</>
+
+const steps = [
+	{
+		Problem: () => <>
+			<Par>Stel dat de compressor <em>wel</em> isentroop was. Bereken in dit geval de temperatuur van de lucht aan de uitgang van de compressor.</Par>
+			<InputSpace>
+				<Par>
+					<FloatUnitInput id="T2p" prelabel={<M>T_(2')=</M>} label="Theoretische temperatuur na compressor" size="s" />
+				</Par>
+			</InputSpace>
+		</>,
+		Solution: () => {
+			const { k, p1, p2, T1, T2p } = useCorrect()
+			return <Par>Poisson's wet zegt dat in dit geval <BM>\frac(T_1^n)(p_1^(n-1)) = \frac(T_(2')^n)(p_2^(n-1)).</BM> Het oplossen van de theoretische temperatuur <M>T_(2')</M> gaat via
+			<BM>T_(2')^n = T_1^n \frac(p_2^(n-1))(p_1^(n-1)) = T_1^n \left(\frac(p_2)(p_1)\right)^(n-1),</BM>
+				<BM>T_(2') = \left(T_1^n \left(\frac(p_2)(p_1)\right)^(n-1)\right)^(\frac(1)(n)) = T_1 \left(\frac(p_2)(p_1)\right)^(\frac(n-1)(n)) = {T1.float} \left(\frac{p2.float}{p1.float}\right)^(\frac({k}-1)({k})) = {T2p}.</BM>
+				Dit is de temperatuur na de compressor als de compressor isentroop zou werken.</Par>
+		},
+	},
+	{
+		Problem: () => <>
+			<Par>Bereken de specifieke technische arbeid van de compressor in dit theoretische isentrope geval.</Par>
+			<InputSpace>
+				<Par>
+					<FloatUnitInput id="wti" prelabel={<M>w_(t_i)=</M>} label="Theoretische specifieke technische arbeid" size="s" />
+				</Par>
+			</InputSpace>
+		</>,
+		Solution: () => {
+			const { cp, T1, T2p, wti } = useCorrect()
+			return <>
+				<Par>Bij de compressor wordt geen warmte toegevoerd, dus <M>q = 0</M>. De technische arbeid volgt vanuit de eerste hoofdwet als
+				<BM>w_t = q - \Delta h = -\Delta h = -c_p \left(T_2 - T_1\right).</BM>
+				Dit geldt zowel voor het theoretische isentrope geval als voor de werkelijkheid. Zo vinden we
+				<BM>w_(t_i) = -c_p \left(T_(2') - T_1\right) = -{cp.float} \cdot \left({T2p.float} - {T1.float}\right) = {wti}.</BM>
+				</Par>
+			</>
+		},
+	},
+	{
+		Problem: () => <>
+			<Par>Gebruik het isentropisch rendement om de werkelijke specifieke technische arbeid van de compressor te berekenen.</Par>
+			<InputSpace>
+				<Par>
+					<FloatUnitInput id="wt" prelabel={<M>w_t =</M>} label="Specifieke technische arbeid" size="s" />
+				</Par>
+			</InputSpace>
+		</>,
+		Solution: () => {
+			const { wt, wti, etai } = useCorrect()
+			return <Par>Bij een compressor is de technische arbeid in het theoretische isentrope geval altijd kleiner dan de technische arbeid in werkelijkheid. Het isentrope rendement van een compressor is dus gedefinieerd als
+					<BM>\eta_i = \frac(w_(t_i))(w_t).</BM>
+					Dit oplossen voor <M>w_t</M> geeft
+					<BM>w_t = \frac(w_(t_i))(\eta_i) = \frac{wti.float}{etai.float} = {wt}.</BM>
+					Deze technische arbeid is inderdaad hoger dan de technische arbeid in het isentrope geval.</Par>
+		},
+	},
+	{
+		Problem: () => <>
+			<Par>Bereken de specifieke technische arbeid van de compressor in dit theoretische isentrope geval.</Par>
+			<InputSpace>
+				<Par>
+					<FloatUnitInput id="T2" prelabel={<M>T_2=</M>} label="Temperatuur na compressor" size="s" />
+				</Par>
+			</InputSpace>
+		</>,
+		Solution: () => {
+			const { cp, T1, T2, T2p, wt, etai } = useCorrect()
+			return <>
+				<Par>Voor de werkelijke situatie geldt ook
+					<BM>w_t = -\Delta h = -c_p \left(T_2 - T_1\right).</BM>
+					Dit oplossen voor <M>T_2</M> geeft
+					<BM>T_2 = T_1 - \frac(w_t)(c_p) = {T1.float} - \frac{wt.float}{cp.float} = {T2}.</BM>
+					Dit is iets hoger dan de temperatuur in het isentrope geval, wat logisch is: extra frictie laat de temperatuur iets stijgen.</Par>
+				<SubHead>Short-cut</SubHead>
+				<Par>We hadden eventueel ook kunnen gebruiken dat
+				<BM>\eta_i = \frac(w_(t_i))(w_t) = \frac(c_p \left(T_(2') - T_1\right))(c_p \left(T_2 - T_1\right)) = \frac(T_(2') - T_1)(T_2 - T_1) = \frac({T2p.float} - {T1.float})({T2.float} - {T1.float}) = {etai}.</BM>
+				Als we dit oplossen voor <M>T_2</M> vinden we
+				<BM>T_2 = T_1 + \frac(T_(2') - T_1)(\eta_i) = {T1.float} + \frac({T2p.float} - {T1.float})({etai.float}) = {T2}.</BM>
+				Dan hadden we dus niet de technische arbeiden uit hoeven te rekenen.
+				</Par>
+			</>
+		},
+	},
+]
+
+const getFeedback = (exerciseData) => {
+	return getDefaultFeedback(['T2p', 'wti', 'wt', 'T2'], exerciseData)
+}
+
---
+++ shared/edu/exercises/exercises/useIsentropicEfficiencyCompressor2.js
@@ -0,0 +1,58 @@
+const { getStepExerciseProcessor } = require('../util/stepExercise')
+const { combinerAnd } = require('../../../skillTracking')
+const { checkParameter } = require('../util/check')
+const { air: { k, cp } } = require('../../../data/gasProperties')
+const { getCycle } = require('./support/gasTurbineCycle')
+
+const data = {
+	skill: 'useIsentropicEfficiency',
+	setup: combinerAnd('poissonsLaw', 'calculateSpecificHeatAndMechanicalWork', 'solveLinearEquation', 'calculateSpecificHeatAndMechanicalWork'),
+	steps: ['poissonsLaw', 'calculateSpecificHeatAndMechanicalWork', 'solveLinearEquation', 'calculateSpecificHeatAndMechanicalWork'],
+
+	equalityOptions: {
+		default: {
+			relativeMargin: 0.01,
+			significantDigitMargin: 1,
+		},
+	},
+}
+
+function generateState() {
+	let { p1, T1, p2, etai } = getCycle()
+	p1 = p1.setDecimals(0).roundToPrecision().setMinimumSignificantDigits(2)
+	p2 = p2.setDecimals(0).roundToPrecision().setMinimumSignificantDigits(2)
+	T1 = T1.setDecimals(0).roundToPrecision()
+	etai = etai.setUnit('%').setDecimals(0).roundToPrecision()
+	return { p1, p2, T1, etai }
+}
+
+function getCorrect({ p1, p2, T1, etai }) {
+	etai = etai.simplify()
+	const T2p = T1.multiply(p2.divide(p1).float.toPower(1 - 1 / k.number)).setDecimals(0)
+	const wti = cp.multiply(T1.subtract(T2p)).setUnit('J/kg')
+	const wt = wti.divide(etai).setUnit('J/kg')
+	const T2 = T1.subtract(wt.divide(cp)).setUnit('K').setDecimals(0)
+	return { k, cp, p1, p2, T1, T2, T2p, wt, wti, etai }
+}
+
+function checkInput(state, input, step, substep) {
+	const correct = getCorrect(state)
+	switch (step) {
+		case 1:
+			return checkParameter('T2p', correct, input, data.equalityOptions)
+		case 2:
+			return checkParameter('wti', correct, input, data.equalityOptions)
+		case 3:
+			return checkParameter('wt', correct, input, data.equalityOptions)
+		default:
+			return checkParameter('T2', correct, input, data.equalityOptions)
+	}
+}
+
+module.exports = {
+	data,
+	generateState,
+	processAction: getStepExerciseProcessor(checkInput, data),
+	checkInput,
+	getCorrect,
+}
