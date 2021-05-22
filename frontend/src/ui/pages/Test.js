dd3f50e85f063c9d69e688fe317feb17a8f19408
Hildo Bijl <hildobijl@gmail.com>
2021-05-22 15:47:50 +0200

    Give Test page its proper name

--- frontend/src/ui/pages/Test.js
+++ frontend/src/ui/pages/Test.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Par, Head } from 'ui/components/containers'
 import { BM } from 'ui/components/equations'
 
-export default function Feedback() {
+export default function Test() {
 	return (
 		<>
 			<Par>Dit is een testpagina. Hij wordt gebruikt om simpele dingen te testen en te kijken hoe ze werken. Vaak staat er willekeurige zooi op.</Par>
