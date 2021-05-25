2441d2589d39c03e72d40d39497331bb7721133a
Hildo Bijl <hildobijl@gmail.com>
2021-05-25 19:14:37 +0200

    Adjust keyboard unit test to updated keyboards

--- frontend/src/ui/form/Keyboard/keyboards/keyboards.test.js
+++ frontend/src/ui/form/Keyboard/keyboards/keyboards.test.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import { noop } from 'step-wise/util/functions'
+
+import theme from 'ui/theme'
 
 import keyboards, { tabs } from './index'
 
@@ -16,12 +21,16 @@ describe('Check all keyboards:', () => {
 			it('has a tab export', () => {
 				expect(keyboard.tab).toBeTruthy()
 			})
-			it('has a Buttons export that is a function', () => {
-				expect(typeof keyboard.Buttons).toBe('function')
+			it('has a Layout export that is a function', () => {
+				expect(typeof keyboard.Layout).toBe('function')
 			})
 			it('renders properly', () => {
-				const Buttons = keyboard.Buttons
-				render(<Buttons />)
+				const Layout = keyboard.Layout
+				render(
+					<ThemeProvider theme={theme}>
+						<Layout settings={{}} keyFunction={noop} keySettings={{}} />
+					</ThemeProvider>
+				)
 			})
 		})
 	})
