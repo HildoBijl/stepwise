905cfcdf680a1782418b7500ffcf6fdc76b6eaa9
Jan Heuermann <jan@jotaen.net>
2021-06-25 16:12:01 +0200

    Rename `getUserIdOrThrow` into `getCurrentUserId`

--- api/src/server/index.js
+++ api/src/server/index.js
@@ -63,19 +63,23 @@ const createServer = ({
 			 * Returns the id of the currently logged in user, or throws an error otherwise.
 			 * Beware: this doesn’t guarantee you that the user still exists in the DB!
 			 */
-			getUserIdOrThrow: () => {
-				if (req.session.principal && req.session.principal.id)
-					return req.session.principal.id
-				throw new AuthenticationError('No user is logged in.')
+			getCurrentUserId: () => {
+				if (!req.session.principal || !req.session.principal.id)
+					throw new AuthenticationError('No user is logged in.')
+				return req.session.principal.id
 			},
 
 			/**
-			 * Returns the currently logged in user object, or throws an exception otherwise.
+			 * Returns the currently logged in user object, or throws an error otherwise.
 			 */
 			getCurrentUser: async () => {
-				if (req.session.principal && req.session.principal.id)
-					return await database.User.findByPk(req.session.principal.id)
-				throw new AuthenticationError('No user is logged in.')
+				if (!req.session.principal || !req.session.principal.id)
+					throw new AuthenticationError('No user is logged in.')
+				const user = await database.User.findByPk(req.session.principal.id)
+				if (!user) {
+					throw new AuthenticationError('No such user in the system.')
+				}
+				return user
 			},
 
 			/**
