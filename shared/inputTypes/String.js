893bbfd89620b75e534b87410c26347890a7e87b
Hildo Bijl <hildobijl@gmail.com>
2021-12-01 18:39:01 +0100

    Fix String input type storage: not as object anymore but as string

--- shared/inputTypes/String.js
+++ shared/inputTypes/String.js
@@ -1,26 +1,36 @@
 const { isNumber } = require('../util/numbers')
 
-function isFOofType(param) {
-	return typeof param === 'string' && !isNumber(param)
+function isFOofType(str) {
+	return typeof str === 'string' && !isNumber(str)
 }
 module.exports.isFOofType = isFOofType
 
-function FOtoIO(param) {
-	return { value: param }
+function FOtoIO(str) {
+	return str
 }
 module.exports.FOtoIO = FOtoIO
 
-function IOtoFO({ value }) {
-	return value
+function IOtoFO(str) {
+	// Check for legacy cases. Previously strings were stored as { type: 'String', value: { value: 'the actual string' } }. It was rather dumb. Check if this is in that old format.
+	if (str.value !== undefined)
+		return str.value
+
+	// Normal case.
+	return str
 }
 module.exports.IOtoFO = IOtoFO
 
 function getEmpty() {
-	return { value: '' }
+	return ''
 }
 module.exports.getEmpty = getEmpty
 
-function isEmpty({ value }) {
+function isEmpty(str) {
+	// Check for legacy cases. (See above.)
+	if (str.value !== undefined)
+		return isEmpty(str.value)
+
+	// Normal case.
 	if (typeof value !== 'string')
 		throw new Error(`Invalid type: expected a string but received "${JSON.stringify(value)}".`)
 	return value === ''
