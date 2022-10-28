9f6137d6a45c1be8ce3941f2f4d23dcefb4b05f6
Hildo Bijl <hildobijl@gmail.com>
2022-10-28 17:40:24 +0200

    Add migration for GroupExerciseEvents table

---
+++ api/migrations/012_create-table-groupExerciseEvent.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+	up: async (queryInterface) => {
+		await queryInterface.createTable('groupExerciseEvents', {
+			id: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4,
+				allowNull: false,
+				primaryKey: true,
+			},
+			groupExerciseSampleId: {
+				type: DataTypes.UUID,
+				references: {
+					model: 'groupExerciseSamples',
+					key: 'id',
+				},
+				onUpdate: 'cascade',
+				onDelete: 'cascade',
+			},
+			progress: {
+				type: DataTypes.JSON,
+				allowNull: false,
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+			updatedAt: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+		})
+	},
+
+	down: async (queryInterface) => {
+		queryInterface.dropTable('groupExerciseEvents')
+	},
+
+}
