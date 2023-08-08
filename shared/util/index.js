10774e4aff8dab15d1312982e36b8d1d9a522113
Hildo Bijl <hildobijl@gmail.com>
2023-08-08 12:48:20 +0200

    Add central gathering and exporting index file in shared/util

---
+++ shared/util/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	...require('./numbers'),
+	...require('./strings'),
+	...require('./objects'),
+	...require('./arrays'),
+	...require('./functions'),
+	...require('./sets'),
+
+	...require('./errors'),
+	...require('./date'),
+	...require('./random'),
+
+	...require('./maths'),
+	...require('./interpolation'),
+}
