c6eaceb8443b16fb8fb9fa8ed85fcd46ca64eda5
Hildo Bijl <hildobijl@gmail.com>
2023-10-22 17:55:50 +0200

    Tiny regex change to prevent eslint warning

--- frontend/src/ui/components/text/inline/Link.js
+++ frontend/src/ui/components/text/inline/Link.js
@@ -6,7 +6,7 @@ const useStyles = makeStyles((theme) => ({
 	link: {}, // Nothing on top of the already defined a-settings.
 }))
 
-const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export function Link({ children, className, style, to }) {
 	// Turn email addresses into an email link.
 	if (typeof to === 'string' && to.match(emailRegEx))
