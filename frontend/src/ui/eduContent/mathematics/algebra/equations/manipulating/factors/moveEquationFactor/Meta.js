0617e83e3c34b8f08da6e9aad40736c4fa9d005c
Hildo Bijl <hildobijl@gmail.com>
2024-08-30 13:02:23 +0200

    Fix minor typo in an equation

--- frontend/src/ui/eduContent/mathematics/algebra/equations/manipulating/factors/moveEquationFactor/Meta.js
+++ frontend/src/ui/eduContent/mathematics/algebra/equations/manipulating/factors/moveEquationFactor/Meta.js
@@ -6,6 +6,6 @@ import { SkillLink } from 'ui/eduTools'
 
 export function Meta() {
 	return <Translation entry="learningGoals">
-		<Par>In this skill you learn to <Term>move a factor to the other side of an equation</Term>. If you have an equation like <M>2 = \frac(3)(x)</M> you will be able to bring <M>x</M> to the other side to get <M>2x = 3</M>, or to then bring <M>2</M> to the other side to get <M>x = \frac(2)(3)</M>. This includes understanding the idea behind this: <SkillLink skillId="multiplyBothEquationSides">multiplying both sides of an equation by a factor</SkillLink> and subsequently <SkillLink skillId="cancelFractionFactors">canceling fraction factors</SkillLink>. The skill does not include dealing with <SkillLink skillId="multiplyAllEquationTerms">equations with multiple terms</SkillLink>.</Par>
+		<Par>In this skill you learn to <Term>move a factor to the other side of an equation</Term>. If you have an equation like <M>2 = \frac(3)(x)</M> you will be able to bring <M>x</M> to the other side to get <M>2x = 3</M>, or to then bring <M>2</M> to the other side to get <M>x = \frac(3)(2)</M>. This includes understanding the idea behind this: <SkillLink skillId="multiplyBothEquationSides">multiplying both sides of an equation by a factor</SkillLink> and subsequently <SkillLink skillId="cancelFractionFactors">canceling fraction factors</SkillLink>. The skill does not include dealing with <SkillLink skillId="multiplyAllEquationTerms">equations with multiple terms</SkillLink>.</Par>
 	</Translation>
 }
