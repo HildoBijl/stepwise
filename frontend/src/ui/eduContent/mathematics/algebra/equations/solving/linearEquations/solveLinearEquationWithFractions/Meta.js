05647a8c2b1d7665e88607598aa1bd266dc3b16c
Hildo Bijl <hildobijl@gmail.com>
2024-09-10 12:05:28 +0200

    Minor fix in solveLinearEquationWithFractions Meta page

--- frontend/src/ui/eduContent/mathematics/algebra/equations/solving/linearEquations/solveLinearEquationWithFractions/Meta.js
+++ frontend/src/ui/eduContent/mathematics/algebra/equations/solving/linearEquations/solveLinearEquationWithFractions/Meta.js
@@ -6,6 +6,6 @@ import { SkillLink } from 'ui/eduTools'
 
 export function Meta() {
 	return <Translation entry="learningGoals">
-		<Par>In this skill you learn to <Term>solve a linear equation that has fractions</Term> with the unknown in the denominator. For an equation like <M>\frac(2)(x+3)=\frac(5)(x+7)</M> you will be able to find the solution <M>x=-1/3</M>. This includes <SkillLink skillId="multiplyAllEquationTerms">multiplying all equation terms</SkillLink> by a factor to get <M>x</M> out of any denominator, and subsequently <SkillLink skillId="solveLinearEquation">solving the resulting linear equation</SkillLink> in the default way. Effectively, this allows the solution of any type of linear equation.</Par>
+		<Par>In this skill you learn to <Term>solve a linear equation that has fractions</Term> with the unknown in the denominator. For an equation like <M>\frac(2)(x+3)=\frac(5)(x+7)</M> you will be able to find the solution <M>x=-1/3</M>. This includes <SkillLink skillId="moveEquationTerm">moving equation factors</SkillLink> to get <M>x</M> out of any denominator, and subsequently <SkillLink skillId="solveLinearEquation">solving the resulting linear equation</SkillLink> in the default way. Effectively, this allows the solution of any type of linear equation.</Par>
 	</Translation>
 }
