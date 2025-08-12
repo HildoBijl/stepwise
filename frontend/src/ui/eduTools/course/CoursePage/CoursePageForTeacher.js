4d043aa64c2d1b6520b83002365b628e954bce1a
Hildo Bijl <hildobijl@gmail.com>
2025-08-12 15:09:03 +0200

    Update to the temperorary Course Teacher pages

--- frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
+++ frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
@@ -1,3 +1,7 @@
 export function CoursePageForTeacher() {
-	return <p>The teacher interface for a course is still under development...</p>
+	return <>
+		<p>The teacher interface for a course is still under development...</p>
+		<p>In the future, this will be the place to inspect the students in your course. How far along are they? You can get the full overview, or zoom in on a specific student.</p>
+		<p>Do you have any wishes for functionalities here? Let me know! Then I'll make sure to implement them along the way.</p>
+	</>
 }
--- frontend/src/ui/eduTools/course/CourseSettingsPage/CourseSettingsPageForTeacher.js
+++ frontend/src/ui/eduTools/course/CourseSettingsPage/CourseSettingsPageForTeacher.js
@@ -1,3 +1,7 @@
 export function CourseSettingsPageForTeacher() {
-	return <p>The course settings page for teachers is still under development...</p>
+	return <>
+		<p>The course settings page for teachers is still under development...</p>
+		<p>In the future, this will be where you can add teachers to the course, or remove yourself as teacher.</p>
+		<p>Do you have any wishes for functionalities here? Let me know! Then I'll make sure to implement them along the way.</p>
+	</>
 }
