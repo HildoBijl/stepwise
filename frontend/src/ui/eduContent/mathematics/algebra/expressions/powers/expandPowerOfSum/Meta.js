73a5324a54b4b449cae23c4f0faad45192a8bbc6
Hildo Bijl <hildobijl@gmail.com>
2025-08-25 17:23:00 +0200

    Adjust the expandPowerOfSum Meta page to cut out negative powers

--- frontend/src/ui/eduContent/mathematics/algebra/expressions/powers/expandPowerOfSum/Meta.js
+++ frontend/src/ui/eduContent/mathematics/algebra/expressions/powers/expandPowerOfSum/Meta.js
@@ -6,6 +6,6 @@ import { SkillLink } from 'ui/eduTools'
 
 export function Meta() {
 	return <Translation entry="learningGoals">
-		<Par>In this skill you learn to <Term>expand brackets for a power of a sum</Term>. If you see an expression like <M>\left(2x-3\right)^3</M> you can turn this into <M>8x^3 - 36x^2 + 54x - 27</M>. This includes setting up the right format, taking <SkillLink skillId="simplifyProductOfPowers">powers of each of the terms</SkillLink>, looking up the right multiplication coefficients, and eventually <SkillLink skillId="simplifyNumberProduct">simplifying all individual terms</SkillLink>. This skill also concerns negative powers like <M>\left(3-2x\right)^(-3)</M>. It only considers sums with two terms, not dealing with expansions like <M>\left(x^2 + 2x + 1\right)^3</M> or similar.</Par>
+		<Par>In this skill you learn to <Term>expand brackets for a power of a sum</Term>. If you see an expression like <M>\left(2x-3\right)^3</M> you can turn this into <M>8x^3 - 36x^2 + 54x - 27</M>. This includes setting up the right format, taking <SkillLink skillId="simplifyProductOfPowers">powers of each of the terms</SkillLink>, looking up the right multiplication coefficients, and eventually <SkillLink skillId="simplifyNumberProduct">simplifying all individual terms</SkillLink>. This skill only considers sums with two terms, not dealing with expansions like <M>\left(x^2 + 2x + 1\right)^3</M> or similar. It also does not consider <SkillLink skillId="rewriteNegativePower">negative powers</SkillLink>.</Par>
 	</Translation>
 }
