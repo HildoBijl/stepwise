22c60f159c2b94d1dcbce0f574655a28bdf6cf29
Hildo Bijl <hildobijl@gmail.com>
2025-08-28 10:28:01 +0200

    Add rewriteNegativePower Theory page (untranslated)

--- shared/eduTools/skills/skillTree/skillTree.js
+++ shared/eduTools/skills/skillTree/skillTree.js
@@ -113,6 +113,10 @@ const skillStructure = {
 				},
 				rewriteNegativePower: {
 					name: 'Rewrite negative power',
+					prerequisites: ['rewritePower', 'multiplyDivideFractions'],
+					// examples: ['powerToFraction'],
+					// exercises: ['powerToFraction', 'fractionToPower'],
+					// exercises: ['powerToFraction', 'fractionToPower'],
 				},
 			},
 			roots: {
