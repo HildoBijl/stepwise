ea32db2ccb907e2b46e5de695a807f7599f6b97b
Hildo Bijl <hildobijl@gmail.com>
2025-09-24 12:19:07 +0200

    Add processing to course students (their skills)

--- frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
+++ frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
@@ -1,6 +1,10 @@
 import { Fragment, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
+import { arraysToObject, keysToObject } from 'step-wise/util'
+import { processSkillDataSet } from 'step-wise/skillTracking'
+import { skillTree, includePrerequisitesAndLinks, processSkill, getDefaultSkillData, getCourseOverview } from 'step-wise/eduTools'
+
 import { TranslationFile, TranslationSection, Translation } from 'i18n'
 import { Par, List } from 'ui/components'
 import { usePaths } from 'ui/routingTools'
@@ -28,13 +32,32 @@ export function CoursePageForTeacher() {
 		</TranslationFile >
 
 	// On students, show the overview.
-	return <StudentOverview students={students} />
+	return <StudentOverview course={course} students={students} />
 }
 
-function StudentOverview({ students }) {
-	students = useMemo(() => [...students].sort((a, b) => a.name.localeCompare(b.name, undefined, { sensitivity: "base" })), [students])
+function StudentOverview({ course, students }) {
+	const overview = useMemo(() => getCourseOverview(course), [course])
+	console.log(overview)
+	const processedStudents = useProcessedStudents(students, overview.all)
+	console.log(processedStudents)
 	return <>
 		<Par>The following students are currently subscribed in the course. Data on their progress will be added in the near future.</Par>
 		<List items={students.map(student => <Fragment key={student.id}>{student.name}</Fragment>)} />
 	</>
+}
+
+function useProcessedStudents(students, skillIds) {
+	return useMemo(() => students.map(student => {
+		// Filter out outdated none-existing skills, process the remaining skills, and turn them into an ID-keyed object (a raw dataset).
+		const skillsProcessed = student.skills.filter(skill => skillTree[skill.skillId]).map(skill => processSkill(skill))
+		const skillsAsObject = arraysToObject(skillsProcessed.map(skill => skill.skillId), skillsProcessed)
+
+		// Add skills that are not in the data set. (These are skills that are not in the database yet.)
+		const allSkillIds = includePrerequisitesAndLinks(skillIds)
+		const skills = keysToObject(allSkillIds, skillId => skillsAsObject[skillId] || getDefaultSkillData(skillId))
+		const skillDataSet = processSkillDataSet(skills, skillTree)
+
+		// Return the resulting skill data set as property of the student.
+		return { ...student, skillDataSet }
+	}), [students, skillIds])
 }
