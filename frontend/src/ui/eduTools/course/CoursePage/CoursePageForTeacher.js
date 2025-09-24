c9439aed2347d67b6ef4664747fd275791977d62
Hildo Bijl <hildobijl@gmail.com>
2025-09-24 13:05:35 +0200

    Add basic student Data Grid for CourseForTeacher page

--- frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
+++ frontend/src/ui/eduTools/course/CoursePage/CoursePageForTeacher.js
@@ -1,13 +1,16 @@
-import { Fragment, useMemo } from 'react'
+import { useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import { DataGrid } from '@mui/x-data-grid'
 
-import { arraysToObject, keysToObject } from 'step-wise/util'
+import { count, arraysToObject, keysToObject } from 'step-wise/util'
 import { processSkillDataSet } from 'step-wise/skillTracking'
 import { skillTree, includePrerequisitesAndLinks, processSkill, getDefaultSkillData, getCourseOverview } from 'step-wise/eduTools'
 
 import { TranslationFile, TranslationSection, Translation } from 'i18n'
-import { Par, List } from 'ui/components'
+import { Par } from 'ui/components'
 import { usePaths } from 'ui/routingTools'
+
+import { getAnalysis } from '../../courses'
 
 import { useCourseData } from '../components'
 
@@ -36,28 +39,53 @@ export function CoursePageForTeacher() {
 }
 
 function StudentOverview({ course, students }) {
+	// Check out the course and define columns based on it.
 	const overview = useMemo(() => getCourseOverview(course), [course])
 	console.log(overview)
-	const processedStudents = useProcessedStudents(students, overview.all)
-	console.log(processedStudents)
-	return <>
-		<Par>The following students are currently subscribed in the course. Data on their progress will be added in the near future.</Par>
-		<List items={students.map(student => <Fragment key={student.id}>{student.name}</Fragment>)} />
-	</>
+	const { blocks } = overview
+	const dgColumns = useMemo(() => [
+		{ field: 'name', headerName: 'Name', width: 150, align: 'left', headerAlign: 'left' },
+		{ field: 'total', headerName: 'Total', width: 80, align: 'center', headerAlign: 'center' },
+		...blocks.map((block, index) => ({ field: `block${index}`, headerName: `${index + 1}`, width: 80, align: 'center', headerAlign: 'center' })),
+	], [blocks])
+
+	// Process the student data and set up rows based on it.
+	const processedStudents = useProcessedStudents(students, overview)
+	const dgRows = useMemo(() => processedStudents.map(student => {
+		return {
+			id: student.id,
+			name: student.name,
+			total: student.numCompleted,
+			...arraysToObject(blocks.map((_, index) => `block${index}`), blocks.map((block, index) => student.numCompletedPerBlock[index])),
+		}
+	}), [processedStudents, blocks])
+
+	return <DataGrid
+		rows={dgRows}
+		columns={dgColumns}
+		initialState={{
+			sorting: { sortModel: [{ field: 'name', sort: 'asc' }] },
+		}}
+		disableColumnMenu
+	/>
 }
 
-function useProcessedStudents(students, skillIds) {
+function useProcessedStudents(students, overview) {
 	return useMemo(() => students.map(student => {
 		// Filter out outdated none-existing skills, process the remaining skills, and turn them into an ID-keyed object (a raw dataset).
 		const skillsProcessed = student.skills.filter(skill => skillTree[skill.skillId]).map(skill => processSkill(skill))
 		const skillsAsObject = arraysToObject(skillsProcessed.map(skill => skill.skillId), skillsProcessed)
 
 		// Add skills that are not in the data set. (These are skills that are not in the database yet.)
-		const allSkillIds = includePrerequisitesAndLinks(skillIds)
+		const allSkillIds = includePrerequisitesAndLinks(overview.all)
 		const skills = keysToObject(allSkillIds, skillId => skillsAsObject[skillId] || getDefaultSkillData(skillId))
-		const skillDataSet = processSkillDataSet(skills, skillTree)
+		const skillsData = processSkillDataSet(skills, skillTree)
 
-		// Return the resulting skill data set as property of the student.
-		return { ...student, skillDataSet }
-	}), [students, skillIds])
+		// Return the resulting skills data set as property of the student and add an analysis for the course.
+		const analysis = getAnalysis(overview, skillsData)
+		const getNumCompleted = skillIds => count(skillIds, (skillId) => analysis.practiceNeeded[skillId] === 0)
+		const numCompleted = getNumCompleted(overview.contents)
+		const numCompletedPerBlock = overview.blocks.map(block => getNumCompleted(block.contents))
+		return { ...student, skillsData, analysis, numCompleted, numCompletedPerBlock }
+	}), [students, overview])
 }
