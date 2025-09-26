6e672cc89c3031e4b79a13427cb23e381c900261
Hildo Bijl <hildobijl@gmail.com>
2025-09-26 10:52:05 +0200

    Add polyfill for TextEncoder so DataGrid doesn't break unit tests

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,2 +1,9 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes. This allows you to do things like expect(element).toHaveTextContent(/react/i). Learn more though https://github.com/testing-library/jest-dom.
 import '@testing-library/jest-dom/extend-expect'
+
+// The MUI DataGrid uses a TextEncoder that is not known to Node. Polyfill it.
+import { TextEncoder, TextDecoder } from 'util'
+if (!global.TextEncoder)
+  global.TextEncoder = TextEncoder
+if (!global.TextDecoder)
+  global.TextDecoder = TextDecoder
