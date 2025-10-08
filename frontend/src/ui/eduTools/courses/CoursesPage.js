61e14e163f8a155e3b8716dda583244dad4e3a53
Hildo Bijl <hildobijl@gmail.com>
2025-10-08 14:25:12 +0200

    Implement teacher tiles on the main courses page dashboard

--- frontend/src/ui/eduTools/courses/CoursesPage.js
+++ frontend/src/ui/eduTools/courses/CoursesPage.js
@@ -9,12 +9,12 @@ import { Translation, TranslationFile } from 'i18n'
 import { Head, LoadingIndicator, ErrorNote } from 'ui/components'
 
 import { getAnalysis } from './util'
-import { Tile, AddCourseTile } from './Tile'
+import { StudentTile, TeacherTile, AddCourseTile } from './Tile'
 
 const translationPath = 'eduTools/pages/coursesPage'
 
 export function CoursesPage() {
-	const myCoursesResult = useMyCoursesQuery()
+	const myCoursesResult = useMyCoursesQuery(true, true)
 
 	// When we don't have the data, show a relevant indication of what's going on.
 	if (myCoursesResult.loading)
@@ -51,18 +51,28 @@ function CoursePageForCourses({ courses }) {
 function StudentCourses({ courses, showHeader }) {
 	return <>
 		{showHeader && <Head><Translation path={translationPath} entry="studentCourses">Courses where you are a student</Translation></Head>}
-		<CourseList courses={courses} showAddButton={true} />
+		<StudentCourseList courses={courses} showAddButton={true} />
 	</>
 }
 
 function TeacherCourses({ courses, showHeader }) {
+	if (courses.length === 0)
+		return null
 	return <>
 		{showHeader && <Head><Translation path={translationPath} entry="teacherCourses">Courses where you are a teacher</Translation></Head>}
-		<CourseList courses={courses} />
+		<TeacherCourseList courses={courses} />
 	</>
 }
 
-function CourseList({ courses, showAddButton }) {
+// Define the style for tile lists.
+const coursesStyle = {
+	display: 'grid',
+	gap: '1rem',
+	gridTemplateColumns: 'repeat(auto-fill, minmax(11rem, 1fr))',
+	gridAutoRows: '8.5rem',
+}
+
+function StudentCourseList({ courses, showAddButton }) {
 	// Load all the skills data for the courses and use it to determine which skills need practice.
 	const sortedCourses = useMemo(() => [...courses].sort((c1, c2) => new Date(c1.subscribedOn) - new Date(c2.subscribedOn)), [courses]) // Sort by subscription date, so that later courses come at the end.
 	const processedCourses = useMemo(() => sortedCourses.map(rawCourse => processCourse(rawCourse)), [sortedCourses])
@@ -71,37 +81,43 @@ function CourseList({ courses, showAddButton }) {
 	const analyses = useMemo(() => processedCourses.map(processedCourse => getAnalysis(processedCourse, skillsData)), [processedCourses, skillsData])
 
 	// Render all the tiles with corresponding data.
-	const coursesStyle = {
-		display: 'grid',
-		gap: '1rem',
-		gridTemplateColumns: 'repeat(auto-fill, minmax(11rem, 1fr))',
-		gridAutoRows: '8.5rem',
-	}
-	return <>
-		<TranslationFile path={translationPath}>
-			{sortedCourses.length > 0 ?
+	return <TranslationFile path={translationPath}>
+		{sortedCourses.length > 0 ?
+			<Box sx={coursesStyle}>
+				{sortedCourses.map((course, index) => <StudentTile
+					key={course.id}
+					course={course}
+					skillsTotal={processedCourses[index].contents.length}
+					skillsDone={analyses[index] ? count(processedCourses[index].contents, (skillId) => analyses[index].practiceNeeded[skillId] === 0) : '0'}
+					recommendation={analyses[index]?.recommendation}
+				/>)}
+				{showAddButton && <AddCourseTile />}
+			</Box>
+			:
+			<>
+				<Translation path={translationPath} entry="noSubscribedCourses">
+					<Alert severity="info" sx={{ mb: 2 }}>
+						<AlertTitle>You haven't subscribed to a course yet!</AlertTitle>
+						Pick a course from the available list to start learning. No worries: it's all free and you can add/remove as many as you like.
+					</Alert>
+				</Translation>
 				<Box sx={coursesStyle}>
-					{sortedCourses.map((course, index) => <Tile
-						key={course.id}
-						course={course}
-						skillsTotal={processedCourses[index].contents.length}
-						skillsDone={analyses[index] ? count(processedCourses[index].contents, (skillId) => analyses[index].practiceNeeded[skillId] === 0) : '0'}
-						recommendation={analyses[index]?.recommendation}
-					/>)}
 					{showAddButton && <AddCourseTile />}
 				</Box>
-				:
-				<>
-					<Translation path={translationPath} entry="noSubscribedCourses">
-						<Alert severity="info" sx={{ mb: 2 }}>
-							<AlertTitle>You haven't subscribed to a course yet!</AlertTitle>
-							Pick a course from the available list to start learning. No worries: it's all free and you can add/remove as many as you like.
-						</Alert>
-					</Translation>
-					<Box sx={coursesStyle}>
-						{showAddButton && <AddCourseTile />}
-					</Box>
-				</>}
-		</TranslationFile >
-	</>
+			</>}
+	</TranslationFile>
+}
+
+function TeacherCourseList({ courses }) {
+	// Load all the skills data for the courses and use it to determine which skills need practice.
+	const sortedCourses = useMemo(() => [...courses].sort((c1, c2) => new Date(c1.subscribedOn) - new Date(c2.subscribedOn)), [courses]) // Sort by subscription date, so that later courses come at the end.
+
+	return <TranslationFile path={translationPath}>
+		<Box sx={coursesStyle}>
+			{sortedCourses.map(course => <TeacherTile
+				key={course.id}
+				course={course}
+			/>)}
+		</Box>
+	</TranslationFile>
 }
--- frontend/src/ui/eduTools/courses/Tile.js
+++ frontend/src/ui/eduTools/courses/Tile.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Tooltip, Box, alpha } from '@mui/material'
 
-import { skillTree } from 'step-wise/eduTools'
+import { skillTree, processCourse } from 'step-wise/eduTools'
 
-import { useTranslator } from 'i18n'
+import { useTranslator, Translation, Plurals } from 'i18n'
 import { notSelectable, linkStyleReset } from 'ui/theme'
 import { usePaths } from 'ui/routingTools'
 import { Button, ProgressIndicator, QuickPractice } from 'ui/components'
@@ -14,7 +14,7 @@ import { getOrganization } from '../organizations'
 import { strFreePractice } from './util'
 
 // Set up style for a tile.
-const getTileStyle = buttonHover => (theme => ({
+const getTileStyle = preventHoverStyle => (theme => ({
 	alignItems: 'stretch',
 	background: alpha(theme.palette.primary.main, 0.03),
 	borderRadius: '0.5rem',
@@ -26,10 +26,41 @@ const getTileStyle = buttonHover => (theme => ({
 	padding: '0.3rem',
 	position: 'relative',
 	...linkStyleReset,
-	'&:hover': { background: alpha(theme.palette.primary.main, buttonHover ? 0.03 : 0.1) },
+	'&:hover': { background: alpha(theme.palette.primary.main, preventHoverStyle ? 0.03 : 0.1) },
 }))
 
-export function Tile({ course, skillsTotal, skillsDone, recommendation }) {
+
+export function Tile({ course, preventHoverStyle, children }) {
+	const translate = useTranslator()
+	const paths = usePaths()
+
+	const organization = getOrganization(course.organization)
+	return <Link to={paths.course({ courseCode: course.code })} style={{ textDecoration: 'none', ...notSelectable }}>
+		<Box boxShadow={1} sx={getTileStyle(preventHoverStyle)}>
+			<Box component="img" src={organization.logo} alt={`Logo ${organization.name}`} sx={{
+				objectFit: 'contain',
+				opacity: 0.05,
+				position: 'absolute',
+				height: '90%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)',
+				width: '90%',
+				top: '50%',
+				...notSelectable,
+			}} />
+			<Box sx={{ alignItems: 'center', display: 'flex', flexFlow: 'row nowrap', height: '35%' }}>
+				<Box sx={{ fontWeight: 500, textAlign: 'center', width: '100%' }}>
+					{translate(course.name, `${course.organization}.${course.code}.name`, 'eduContent/courseInfo')}
+				</Box>
+			</Box>
+			<Box sx={{ alignItems: 'center', display: 'flex', flexFlow: 'row nowrap', height: '65%', justifyContent: 'space-evenly' }}>
+				{children}
+			</Box>
+		</Box>
+	</Link>
+}
+
+export function StudentTile({ course, skillsTotal, skillsDone, recommendation }) {
 	const translate = useTranslator()
 	const paths = usePaths()
 	const [buttonHover, setButtonHover] = useState(false)
@@ -59,37 +90,63 @@ export function Tile({ course, skillsTotal, skillsDone, recommendation }) {
 			navigate(paths.courseSkill({ courseCode: course.code, skillId: recommendation }))
 	}
 
-	const organization = getOrganization(course.organization)
-	return <Link to={paths.course({ courseCode: course.code })} style={{ textDecoration: 'none', ...notSelectable }}>
-		<Box boxShadow={1} sx={getTileStyle(buttonHover)}>
-			<Box component="img" src={organization.logo} alt={`Logo ${organization.name}`} sx={{
-				objectFit: 'contain',
-				opacity: 0.05,
-				position: 'absolute',
-				height: '90%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)',
-				width: '90%',
-				top: '50%',
-				...notSelectable,
-			}} />
-			<Box sx={{ alignItems: 'center', display: 'flex', flexFlow: 'row nowrap', height: '35%' }}>
-				<Box sx={{ fontWeight: 500, textAlign: 'center', width: '100%' }}>
-					{translate(course.name, `${course.organization}.${course.code}.name`, 'eduContent/courseInfo')}
-				</Box>
-			</Box>
-			<Box sx={{ alignItems: 'center', display: 'flex', flexFlow: 'row nowrap', height: '65%', justifyContent: 'space-evenly' }}>
-				<ProgressIndicator total={skillsTotal} done={skillsDone} size={60} />
-				<Box>
-					<Tooltip title={tooltip} arrow sx={{ maxWidth: '12rem', textAlign: 'center' }}>
-						<Button variant="contained" color="info" onMouseEnter={() => setButtonHover(true)} onMouseLeave={() => setButtonHover(false)} onClick={goToRecommendation} sx={{ borderRadius: '0.5rem', height: '3rem', minWidth: 0, padding: 0, width: '3rem' }}>
-							<QuickPractice />
-						</Button>
-					</Tooltip>
-				</Box>
+	// Render the contents within the tile.
+	return <Tile course={course} preventHoverStyle={buttonHover}>
+		<ProgressIndicator total={skillsTotal} done={skillsDone} size={60} />
+		<Box>
+			<Tooltip title={tooltip} arrow sx={{ maxWidth: '12rem', textAlign: 'center' }}>
+				<Button variant="contained" color="info" onMouseEnter={() => setButtonHover(true)} onMouseLeave={() => setButtonHover(false)} onClick={goToRecommendation} sx={{ borderRadius: '0.5rem', height: '3rem', minWidth: 0, padding: 0, width: '3rem' }}>
+					<QuickPractice />
+				</Button>
+			</Tooltip>
+		</Box>
+	</Tile>
+}
+
+export function TeacherTile({ course }) {
+	// Process the course.
+	const processedCourse = useMemo(() => processCourse(course), [course])
+
+	// Define styles.
+	const containerStyle = { textAlign: 'center' }
+	const numberStyle = {
+		fontSize: '1.6rem',
+		fontWeight: 600,
+		color: theme => theme.palette.primary.main,
+		lineHeight: 1,
+	}
+	const labelStyle = {
+		fontSize: '0.75rem',
+		fontWeight: 500,
+		color: theme => theme.palette.text.secondary,
+		mt: 0.5,
+	}
+
+	// Render the tile part.
+	return <Tile course={course}>
+		<Box sx={containerStyle}>
+			<Box sx={numberStyle}>{processedCourse.contents.length}</Box>
+			<Box sx={labelStyle}>
+				<Translation entry="tiles.skills">
+					<Plurals value={processedCourse.contents.length}>
+						<Plurals.One>Skill</Plurals.One>
+						<Plurals.NotOne>Skills</Plurals.NotOne>
+					</Plurals>
+				</Translation>
 			</Box>
 		</Box>
-	</Link >
+		<Box sx={containerStyle}>
+			<Box sx={numberStyle}>{course.students.length}</Box>
+			<Box sx={labelStyle}>
+				<Translation entry="tiles.students">
+					<Plurals value={course.students.length}>
+						<Plurals.One>Student</Plurals.One>
+						<Plurals.NotOne>Students</Plurals.NotOne>
+					</Plurals>
+				</Translation>
+			</Box>
+		</Box>
+	</Tile >
 }
 
 export function AddCourseTile() {
