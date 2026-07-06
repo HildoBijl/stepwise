9dbfd20e7ae594129667df8cc4d9fac7eb76ced7
Hildo Bijl <hildobijl@gmail.com>
2026-07-06 15:30:05 +0200

    Fix bug in skill tracking inference where prediction became really slow

--- shared/packages/skill-tracking/src/inference.ts
+++ shared/packages/skill-tracking/src/inference.ts
@@ -18,33 +18,33 @@ export function getSetupExpectedValue(setup: SkillSetup, getCoefficients: (skill
 // Find the distribution of a set-up.
 export function getSetupCoefficients(setup: SkillSetup, getCoefficients: (skillId: string) => BernsteinCoefficients, inferenceOrder = defaultInferenceOrder): BernsteinCoefficients {
 	// Note: the following lines are an approximation. Mathematically simple to calculate, but not accurate. It immediately inserts the expected values, instead of expanding the polynomial and using the respective moments.
-	// const expectedValue = getSetupExpectedValue(setup, getCoefficients)
-	// const coefficients = repeat(inferenceOrder + 1, i => (inferenceOrder + 1) * binomial(inferenceOrder, i) * expectedValue ** i * (1 - expectedValue) ** (inferenceOrder - i))
-	// return normalizeBernsteinCoefficients(coefficients)
-
-	// Find the coefficients of the skills in the polynomial.
-	const polynomial = setup.getPolynomialExpression()
-	const skillCoefficients = polynomial.list.map(skillId => getCoefficients(skillId))
-
-	// Precalculate the powers of the polynomial that will be used.
-	const powersOfPolynomial = getPolynomialPowerList(polynomial, inferenceOrder)
-	const powersOfOneMinusPolynomial = getPolynomialPowerList(oneMinusPolynomial(polynomial), inferenceOrder)
-
-	// Precalculate moments that will be needed within the calculation.
-	const dimensions = getDimensions(polynomial.matrix)
-	const moments = repeat(polynomial.list.length, index => {
-		const maxExponent = (dimensions[index] - 1) * inferenceOrder
-		return repeat(maxExponent + 1, exponent => getBernsteinMoment(skillCoefficients[index], exponent))
-	})
-	const getIndividualMoment = (index: number, exponent: number) => moments[index][exponent]
-
-	// Calculate each of the new coefficients.
-	const coefficients = repeat(inferenceOrder + 1, i => {
-		const basisPolynomial = multiplyPolynomials([powersOfPolynomial[i], powersOfOneMinusPolynomial[inferenceOrder - i]], polynomial.list)
-		const expectedBasisPolynomial = substituteIndividualMomentsIntoPolynomial(basisPolynomial, getIndividualMoment, polynomial.list) as number
-		return (inferenceOrder + 1) * binomial(inferenceOrder, i) * expectedBasisPolynomial
-	})
+	const expectedValue = getSetupExpectedValue(setup, getCoefficients)
+	const coefficients = repeat(inferenceOrder + 1, i => (inferenceOrder + 1) * binomial(inferenceOrder, i) * expectedValue ** i * (1 - expectedValue) ** (inferenceOrder - i))
 	return normalizeBernsteinCoefficients(coefficients)
+
+	// // Find the coefficients of the skills in the polynomial.
+	// const polynomial = setup.getPolynomialExpression()
+	// const skillCoefficients = polynomial.list.map(skillId => getCoefficients(skillId))
+
+	// // Precalculate the powers of the polynomial that will be used.
+	// const powersOfPolynomial = getPolynomialPowerList(polynomial, inferenceOrder)
+	// const powersOfOneMinusPolynomial = getPolynomialPowerList(oneMinusPolynomial(polynomial), inferenceOrder)
+
+	// // Precalculate moments that will be needed within the calculation.
+	// const dimensions = getDimensions(polynomial.matrix)
+	// const moments = repeat(polynomial.list.length, index => {
+	// 	const maxExponent = (dimensions[index] - 1) * inferenceOrder
+	// 	return repeat(maxExponent + 1, exponent => getBernsteinMoment(skillCoefficients[index], exponent))
+	// })
+	// const getIndividualMoment = (index: number, exponent: number) => moments[index][exponent]
+
+	// // Calculate each of the new coefficients.
+	// const coefficients = repeat(inferenceOrder + 1, i => {
+	// 	const basisPolynomial = multiplyPolynomials([powersOfPolynomial[i], powersOfOneMinusPolynomial[inferenceOrder - i]], polynomial.list)
+	// 	const expectedBasisPolynomial = substituteIndividualMomentsIntoPolynomial(basisPolynomial, getIndividualMoment, polynomial.list) as number
+	// 	return (inferenceOrder + 1) * binomial(inferenceOrder, i) * expectedBasisPolynomial
+	// })
+	// return normalizeBernsteinCoefficients(coefficients)
 }
 
 // Get the distributions of a skill based only on linked skills, one coefficient array per link.
