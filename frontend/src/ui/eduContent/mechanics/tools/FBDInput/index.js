62b3a903f017c054d3b802e991474aea81f09d26
Hildo Bijl <hildobijl@gmail.com>
2026-08-07 13:39:28 +0200

    Remove dependency on old mechanics package from shared folders

--- frontend/src/ui/eduContent/mechanics/tools/FBDInput/index.js
+++ frontend/src/ui/eduContent/mechanics/tools/FBDInput/index.js
@@ -1,5 +1,3 @@
-export * from 'step-wise/eduContent/mechanics' // Export shared tools too, for easy referencing.
-
 export * from './feedback'
 export { default } from './rendering'
 export * from './rendering'
