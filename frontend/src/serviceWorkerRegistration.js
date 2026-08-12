f171b0709cc7e64c05482ca84d4edd38b58da71f
Hildo Bijl <hildobijl@gmail.com>
2026-08-12 17:20:34 +0200

    Update deployment scripts to use the new Vite bundler

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -23,7 +23,7 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(import.meta.env.BASE_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin from what our page is served on. This might happen if a CDN is used to serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      // Our service worker won't work if Vite's base URL is on a different origin from the page. This could happen when assets are served through a CDN.
       return;
     }
 
