4f3fc9c1d6be33dd3c6c8afaaa15858840683d4d
Hildo Bijl <hildobijl@gmail.com>
2026-08-25 09:29:53 +0200

    Add function to filter by exercise mode to exercise-bundling

--- packages/exercise-bundling/src/exerciseCollection.ts
+++ packages/exercise-bundling/src/exerciseCollection.ts
@@ -1,5 +1,5 @@
-import { isPlainObject } from '@step-wise/js-utils'
-import { type AnyExercise, isExercise } from '@step-wise/exercise-definition'
+import { filterProperties, isPlainObject } from '@step-wise/js-utils'
+import { type AnyExercise, type ExerciseMode, ensureExerciseMode, exerciseSupportsMode, isExercise } from '@step-wise/exercise-definition'
 
 // A keyed collection of exercises.
 export type ExerciseId = string
@@ -15,4 +15,10 @@ export function isExerciseCollection(obj: unknown): obj is ExerciseCollection {
 export function isEmptyExerciseCollection(exerciseCollection?: ExerciseCollection) {
 	if (exerciseCollection === undefined) return true
 	return Object.values(exerciseCollection).length === 0
+}
+
+// Return only the exercises that support the requested mode.
+export function filterExerciseCollectionByMode(exerciseCollection: ExerciseCollection, mode: ExerciseMode): ExerciseCollection {
+	const ensuredMode = ensureExerciseMode(mode)
+	return filterProperties(exerciseCollection, exercise => exerciseSupportsMode(exercise, ensuredMode)) as ExerciseCollection
 }
