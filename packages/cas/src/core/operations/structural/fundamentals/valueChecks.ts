afe38b3f7cb28c0a5d903126de2f45d85d689525
Hildo Bijl <hildobijl@gmail.com>
2026-08-26 16:16:16 +0200

    Improve Typescript type implications for CAS type checks

--- packages/cas/src/core/operations/structural/fundamentals/valueChecks.ts
+++ packages/cas/src/core/operations/structural/fundamentals/valueChecks.ts
@@ -2,29 +2,36 @@ import { ExpressionNode, SignNode, ConstantNode, Integer, Float } from '../../..
 
 import { isIntegerNode, isFloatNode, isConstantNode, isMinus } from './typeChecks'
 
+type ConstantWithValue<Value extends number> = ConstantNode & { readonly value: Value }
+type SignWithNode<Node extends ExpressionNode> = SignNode & { readonly node: Node }
+
 // Specific values
-export function isZero(node: ExpressionNode): node is ConstantNode { return isConstantNode(node) && node.value === 0 }
-export function isOne(node: ExpressionNode): node is ConstantNode { return isConstantNode(node) && node.value === 1 }
-export function isTwo(node: ExpressionNode): node is ConstantNode { return isConstantNode(node) && node.value === 2 }
-export function isMinusOne(node: ExpressionNode): node is SignNode { return isMinus(node) && isOne(node.node) }
+export function isZero(node: ExpressionNode): node is ConstantWithValue<0> { return isConstantNode(node) && node.value === 0 }
+export function isOne(node: ExpressionNode): node is ConstantWithValue<1> { return isConstantNode(node) && node.value === 1 }
+export function isTwo(node: ExpressionNode): node is ConstantWithValue<2> { return isConstantNode(node) && node.value === 2 }
+export function isMinusOne(node: ExpressionNode): node is SignWithNode<ConstantWithValue<1>> { return isMinus(node) && isOne(node.node) }
 
 // Constants
 export function isConstant(node: ExpressionNode): boolean { return isConstantNode(node) || (isMinus(node) && isConstantNode(node.node)) }
 export function isNonNegativeConstant(node: ExpressionNode): node is ConstantNode { return isConstantNode(node) }
-export function isPositiveConstant(node: ExpressionNode): node is ConstantNode { return isConstantNode(node) && node.value > 0 }
-export function isNonPositiveConstant(node: ExpressionNode): node is ConstantNode | SignNode { return isZero(node) || (isMinus(node) && isNonNegativeConstant(node.node)) }
-export function isNegativeConstant(node: ExpressionNode): node is ConstantNode | SignNode { return isMinus(node) && isPositiveConstant(node.node) }
+export function isPositiveConstant(node: ExpressionNode): boolean { return isConstantNode(node) && hasPositiveValue(node) }
+export function isNonPositiveConstant(node: ExpressionNode): boolean { return isZero(node) || (isMinus(node) && isNonNegativeConstant(node.node)) }
+export function isNegativeConstant(node: ExpressionNode): boolean { return isMinus(node) && isConstantNode(node.node) && hasPositiveValue(node.node) }
 
 // Integers
-export function isInteger(node: ExpressionNode): node is Integer | SignNode { return isIntegerNode(node) || (isMinus(node) && isIntegerNode(node.node)) }
+export function isInteger(node: ExpressionNode): node is Integer | SignWithNode<Integer> { return isIntegerNode(node) || (isMinus(node) && isIntegerNode(node.node)) }
 export function isNonNegativeInteger(node: ExpressionNode): node is Integer { return isIntegerNode(node) }
-export function isPositiveInteger(node: ExpressionNode): node is Integer { return isIntegerNode(node) && node.value > 0 }
-export function isNonPositiveInteger(node: ExpressionNode): node is Integer | SignNode { return (isIntegerNode(node) && isZero(node)) || (isMinus(node) && isNonNegativeInteger(node.node)) }
-export function isNegativeInteger(node: ExpressionNode): node is SignNode { return isMinus(node) && isPositiveInteger(node.node) }
+export function isPositiveInteger(node: ExpressionNode): boolean { return isIntegerNode(node) && hasPositiveValue(node) }
+export function isNonPositiveInteger(node: ExpressionNode): boolean { return (isIntegerNode(node) && node.value === 0) || (isMinus(node) && isNonNegativeInteger(node.node)) }
+export function isNegativeInteger(node: ExpressionNode): boolean { return isMinus(node) && isIntegerNode(node.node) && hasPositiveValue(node.node) }
 
 // Floats
-export function isFloat(node: ExpressionNode): node is Float | SignNode { return isFloatNode(node) || (isMinus(node) && isFloatNode(node.node)) }
+export function isFloat(node: ExpressionNode): node is Float | SignWithNode<Float> { return isFloatNode(node) || (isMinus(node) && isFloatNode(node.node)) }
 export function isNonNegativeFloat(node: ExpressionNode): node is Float { return isFloatNode(node) }
-export function isPositiveFloat(node: ExpressionNode): node is Float { return isFloatNode(node) && node.value > 0 }
-export function isNonPositiveFloat(node: ExpressionNode): node is Float | SignNode { return (isFloatNode(node) && isZero(node)) || (isMinus(node) && isNonNegativeFloat(node.node)) }
-export function isNegativeFloat(node: ExpressionNode): node is SignNode { return isMinus(node) && isPositiveFloat(node.node) }
+export function isPositiveFloat(node: ExpressionNode): boolean { return isFloatNode(node) && hasPositiveValue(node) }
+export function isNonPositiveFloat(node: ExpressionNode): boolean { return (isFloatNode(node) && node.value === 0) || (isMinus(node) && isNonNegativeFloat(node.node)) }
+export function isNegativeFloat(node: ExpressionNode): boolean { return isMinus(node) && isFloatNode(node.node) && hasPositiveValue(node.node) }
+
+function hasPositiveValue(node: ConstantNode): boolean {
+	return node.value > 0
+}
