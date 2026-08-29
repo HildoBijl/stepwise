056c31daa3c3bb7934bda9d0bd137061f9527e31
Hildo Bijl <hildobijl@gmail.com>
2026-08-29 10:35:28 +0200

    Set up types for a new sparse polynomial package set-up

--- packages/polynomials/src/types.ts
+++ packages/polynomials/src/types.ts
@@ -1,7 +1,7 @@
-import { type NestedArray } from '@step-wise/js-utils'
-
-export type PolynomialCoefficients = number | NestedArray<number>
+export type PolynomialExponents = readonly number[]
+export type PolynomialTerm = Readonly<{ coefficient: number, exponents: PolynomialExponents }>
+export type PolynomialTerms = readonly PolynomialTerm[]
 export type PolynomialVariables = readonly string[]
-export type Polynomial = Readonly<{ coefficients: PolynomialCoefficients, variables: PolynomialVariables }>
+export type Polynomial = Readonly<{ terms: PolynomialTerms, variables: PolynomialVariables }>
 export type PolynomialValues = Readonly<Record<string, number>>
 export type PolynomialComparisonOptions = Readonly<{ allowVariableReordering?: boolean }>
